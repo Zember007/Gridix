63656dbc563a853b1ea13411951f4c26dc29463e
Georgii Borisov <goraegorgora@mail.ru>
2025-09-19 13:39:53 +0400

    fix

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -229,7 +229,7 @@ const Index = () => {
       >
         <iframe
           id="gridix-widget"
-          src={`http://gridix.live/embed/project/85a56cab-d420-4d3e-aa37-2c21bce021a8?lang=${language}`}
+          src={`https://gridix.live/embed/project/85a56cab-d420-4d3e-aa37-2c21bce021a8?lang=${language}`}
           width="100%"
           height="100%"
           >
