76125a15a1c8ee9e0db9f2298c47d16b152b896f
Georgii Borisov <goraegorgora@mail.ru>
2026-01-08 18:12:58 +0400

    edit bitrix

--- src/pages/bitrix/BitrixInstallPage.tsx
+++ src/pages/bitrix/BitrixInstallPage.tsx
@@ -1,35 +1,37 @@
 import { useEffect, useState } from 'react';
 const BitrixInstallPage = () => {
   const [isInitialized, setIsInitialized] = useState(false);
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://api.bitrix24.com/api/v1/';
-        script.async = true;
-        script.onload = () => {
-          const BX24 = (window as any).BX24;
-          if (BX24) {
-            BX24.init(() => {
-              setIsInitialized(true);
-              console.log('BX24 инициализирован');
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://api.bitrix24.com/api/v1/';
+    script.async = true;
+    script.onload = () => {
+      setTimeout(() => {
+        const BX24 = (window as any).BX24;
+        if (BX24) {
+          BX24.init(() => {
+            setIsInitialized(true);
+            console.log('BX24 инициализирован');
 
-              BX24.resizeWindow(1200, 800); // Важно! Без ресайза вкладка может быть крошечной/пустой
-              // BX24.fitWindow(); // альтернатива
-            });
-          } else {
-            console.log('BX24 не инициализирован');
-            setIsInitialized(false);
-          }
-        };
-        document.head.appendChild(script);
-      
-        return () => { document.head.removeChild(script); };
-      }, []);
-    return (
-        <div>
-            Проверка Bitrix24
-            {isInitialized ? 'BX24 инициализирован' : 'BX24 не инициализирован'}
-        </div>
-    );
+            BX24.resizeWindow(1200, 800); // Важно! Без ресайза вкладка может быть крошечной/пустой
+            // BX24.fitWindow(); // альтернатива
+          });
+        } else {
+          console.log('BX24 не инициализирован');
+          setIsInitialized(false);
+        }
+      }, 1000);
+    };
+    document.head.appendChild(script);
+
+    return () => { document.head.removeChild(script); };
+  }, []);
+  return (
+    <div>
+      Проверка Bitrix24
+      {isInitialized ? 'BX24 инициализирован' : 'BX24 не инициализирован'}
+    </div>
+  );
 };
 
 export default BitrixInstallPage;
