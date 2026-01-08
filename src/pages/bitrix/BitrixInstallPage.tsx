30835e7d2319abb6bef8b84cb254923e33cf77c1
Georgii Borisov <goraegorgora@mail.ru>
2026-01-08 18:10:14 +0400

    edit bitrix

--- src/pages/bitrix/BitrixInstallPage.tsx
+++ src/pages/bitrix/BitrixInstallPage.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 const BitrixInstallPage = () => {
+  const [isInitialized, setIsInitialized] = useState(false);
     useEffect(() => {
         const script = document.createElement('script');
         script.src = 'https://api.bitrix24.com/api/v1/';
@@ -8,12 +9,15 @@ const BitrixInstallPage = () => {
           const BX24 = (window as any).BX24;
           if (BX24) {
             BX24.init(() => {
+              setIsInitialized(true);
+              console.log('BX24 инициализирован');
 
               BX24.resizeWindow(1200, 800); // Важно! Без ресайза вкладка может быть крошечной/пустой
               // BX24.fitWindow(); // альтернатива
             });
           } else {
             console.log('BX24 не инициализирован');
+            setIsInitialized(false);
           }
         };
         document.head.appendChild(script);
@@ -23,6 +27,7 @@ const BitrixInstallPage = () => {
     return (
         <div>
             Проверка Bitrix24
+            {isInitialized ? 'BX24 инициализирован' : 'BX24 не инициализирован'}
         </div>
     );
 };
