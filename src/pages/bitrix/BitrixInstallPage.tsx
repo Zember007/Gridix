697e2921f3653df213e240e70c7abdc9db6910bf
Georgii Borisov <goraegorgora@mail.ru>
2026-01-08 18:19:29 +0400

    edit bitrix

--- src/pages/bitrix/BitrixInstallPage.tsx
+++ src/pages/bitrix/BitrixInstallPage.tsx
@@ -2,29 +2,21 @@ import { useEffect, useState } from 'react';
 const BitrixInstallPage = () => {
   const [isInitialized, setIsInitialized] = useState(false);
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://api.bitrix24.com/api/v1/';
-    script.async = true;
-    script.onload = () => {
-      setTimeout(() => {
-        const BX24 = (window as any).BX24;
-        if (BX24) {
-          BX24.init(() => {
-            setIsInitialized(true);
-            console.log('BX24 инициализирован');
 
-            BX24.resizeWindow(1200, 800); // Важно! Без ресайза вкладка может быть крошечной/пустой
-            // BX24.fitWindow(); // альтернатива
-          });
-        } else {
-          console.log('BX24 не инициализирован');
-          setIsInitialized(false);
-        }
-      }, 1000);
-    };
-    document.head.appendChild(script);
+    const BX24 = (window as any).BX24;
+    if (BX24) {
+      BX24.init(() => {
+        setIsInitialized(true);
+        console.log('BX24 инициализирован');
 
-    return () => { document.head.removeChild(script); };
+        BX24.resizeWindow(1200, 800); // Важно! Без ресайза вкладка может быть крошечной/пустой
+        // BX24.fitWindow(); // альтернатива
+      });
+    } else {
+      console.log('BX24 не инициализирован');
+      setIsInitialized(false);
+    }
+    
   }, []);
   return (
     <div>
