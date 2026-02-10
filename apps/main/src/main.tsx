fb4ec7555e11d8abd180f4339f6f36f8fb7c6421
Georgii Borisov <goraegorgora1306@gmail.com>
2026-02-10 16:21:54 +0400

    fix Sentry

--- apps/main/src/main.tsx
+++ apps/main/src/main.tsx
@@ -3,12 +3,14 @@ import App from "@/app/App";
 import "./index.css";
 import * as Sentry from "@sentry/react";
 
-Sentry.init({
-  tracePropagationTargets: [/^https:\/\/app\.gridix\.live/],
-  dsn: "https://52b57de545d74b53fb151945fbf8f82e@o4510747380809728.ingest.de.sentry.io/4510747384938576",
-  integrations: [Sentry.browserTracingIntegration()],
-  tracesSampleRate: 1.0,
-});
+if (import.meta.env.PROD) {
+  Sentry.init({
+    tracePropagationTargets: [/^https:\/\/app\.gridix\.live/],
+    dsn: "https://52b57de545d74b53fb151945fbf8f82e@o4510747380809728.ingest.de.sentry.io/4510747384938576",
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: 1.0,
+  });
+}
 
 
 
