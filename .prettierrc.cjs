b8294e9720262fd2a1fe220d33d331ad520014d9
Sanyo4ek958 <sanyo4ek952@gmail.com>
2026-02-13 14:07:32 +0300

    chore(prettier): add config and ignore

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  arrowParens: "always",
+  endOfLine: "lf",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
