8decbbc7f874d0779416cadef0bb00d4ae038bfe
AnatolyPautovDev <pautov.tolick@yandex.by>
2026-02-20 14:24:35 +0300

    [agent-cabinet] Remove unused tab page components

--- apps/agent-cabinet/src/pages/tabs/PartnerProgramTab.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { PartnerProgram } from "@gridix/partner-program";
-export function PartnerProgramTab() {
-  return _jsx("div", {
-    className: "mx-auto max-w-[1600px] p-4 md:p-6",
-    children: _jsx(PartnerProgram, { navigationMode: "tabs" }),
-  });
-}
